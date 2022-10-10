cf36c1be4a1eb495c14e71dfd820840f60da7bcc
Alex <alex.1989@hotmail.com>
2022-10-10 23:28:21 +0200

    added types

--- shared/src/permissions/helpers.ts
+++ shared/src/permissions/helpers.ts
@@ -1,4 +1,5 @@
-import { Permission, PermissionCodec } from '..';
+import { PermissionCodec } from './PermissionCodec';
+import { Permission } from './permissions';
 
 export const can = (assertedPermission: Permission, encodedPermissions: string) => {
     return PermissionCodec.decode(encodedPermissions)
