014dbb7690ca368e4b384bb64439c7e384d38627
Alex <alex.1989@hotmail.com>
2023-01-28 01:58:35 +0100

    fixed random filename generation

--- server/src/utils/string.ts
+++ server/src/utils/string.ts
@@ -1,4 +1,5 @@
 import { randomBytes } from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
 
 export const randomString = (length?: number) => randomBytes(length ?? 15).toString('base64');
-export const randomFilename = (ext: string, length?: number) => `${randomString(length).replace(/[/+=]/g, '')}.${ext}`;
+export const randomFilename = (ext: string) => `${uuidv4()}.${ext}`;
